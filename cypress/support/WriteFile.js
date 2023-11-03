b98c157b955a8e4ac344c4ad01ee5a284f27f367
pratapghorpade <ghorpadepratap10@gmail.com>
2023-11-03 21:53:45 +0530

    new change auth

--- cypress/support/WriteFile.js
+++ cypress/support/WriteFile.js
@@ -1,27 +1,30 @@
 
-
+import Auth2 from "/home/dharapajwani/APIAutomation/KycApp/cypress/Payload/Auth2.json"
 
 Cypress.Commands.add('WriteFile', (CustID, PanNumber) => {
 
-    cy.writeFile("../KycApp/cypress/fixtures/KycStatus.json", {                                               
-                              customerId: CustID,
-                              panNo: PanNumber,
-                          }) 
+              
 
-    cy.writeFile("../KycApp/cypress/fixtures/CheckOutUrl_Payload.json", {
-                          customerId: CustID,
-                          type: "",
-                          checkoutReturnUrl:  "https://goldenpiplus.com/sovereign-gold-bond",
-                      
-                      })   
+                                        cy.writeFile("../KycApp/cypress/fixtures/KycStatus.json", {                                               
+                                                                customerId: CustID,
+                                                                panNo: PanNumber,
+                                                            }) 
+
+                                        cy.writeFile("../KycApp/cypress/fixtures/CheckOutUrl_Payload.json", {
+                                                            customerId: CustID,
+                                                            type: "",
+                                                            checkoutReturnUrl:  "https://goldenpiplus.com/sovereign-gold-bond",
+                                                        
+                                                        })   
     
-    cy.writeFile("../KycApp/cypress/fixtures/deleteuser.json", 
-                          {
-                          authToken: "ry6GnRCoqFwK971MGZMVJnCHTxuZDGTaafFQmTGK79ePzyzQ2gBXO7AUX8MTPr0U",
-                          apiKey: "deleteCustomerForQa",
-                          apiParam: {
-                          customerId: [CustID]
-                          }
+                                        cy.writeFile("../KycApp/cypress/fixtures/deleteuser.json", 
+                                                            {
+                                                            authToken: Auth2.Auth,
+                                                            apiKey: "deleteCustomerForQa",
+                                                            apiParam: {
+                                                            customerId: [CustID]
+                                                            }
+
+     })
 })
 
-})
